fd093d150ce89c96803d55ddf8194cdf5119001c
gudangpakiah21-svg <gudangpakiah21@gmail.com>
2026-08-30 03:08:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const net = require('net');
 const app = express();
 
+// Menggunakan PORT bawaan Railway (biasanya 3000/8000)
 const WEB_PORT = process.env.PORT || 8081;
 const TCP_PORT = 8881;
 
 app.use(express.json());
 
-// 1. Tampilan sederhananya (Web UI Dashboard)
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -28,15 +28,12 @@ app.get('/', (req, res) => {
   `);
 });
 
-// 2. HTTP Web UI Server Listener
 app.listen(WEB_PORT, () => {
   console.log(`Web UI berjalan di port ${WEB_PORT}`);
 });
 
-// 3. Listener TCP Proxy Sederhana (Port 8881)
 const tcpServer = net.createServer((socket) => {
   console.log('Koneksi TCP masuk dari:', socket.remoteAddress);
-  // Di sini logika untuk penanganan protokol (SOCKS5/Vmess/HTTP Connect) dijalankan
 });
 
 tcpServer.listen(TCP_PORT, () => {
